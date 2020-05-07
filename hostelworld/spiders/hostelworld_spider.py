7e2993ca305f88920e1394ace7b47dd20637dd12
Zack Zbar <zackzbar@gmail.com>
2020-05-07 01:26:29 -0400

    Seemingly fixed issue where some cities had extra line in hostel_urls. Worked on Hoi An and Hue, where Hoi An was having that issue and Hue was not. Saved file for those 2 cities.

--- hostelworld/spiders/hostelworld_spider.py
+++ hostelworld/spiders/hostelworld_spider.py
@@ -6,12 +6,12 @@ import math
 class HostelworldSpider(Spider) :
     name = 'hostelworld_spider'
     allowed_urls = ['https://www.hostelworld.com']
-    start_urls = [#'https://www.hostelworld.com/hostels/Bangkok/Thailand',
-                     # 'https://www.hostelworld.com/hostels/Hanoi/Vietnam', 
+    start_urls = [#'https://www.hostelworld.com/hostels/Hanoi/Vietnam',
+                     # 'https://www.hostelworld.com/hostels/Bangkok/Thailand']#,
                      # 'https://www.hostelworld.com/hostels/Ho-Chi-Minh/Vietnam',
-                     'https://www.hostelworld.com/hostels/Hoi-An/Vietnam']#,
+                     'https://www.hostelworld.com/hostels/Hoi-An/Vietnam',
                      # 'https://www.hostelworld.com/hostels/Da-Nang/Vietnam',
-                     # 'https://www.hostelworld.com/hostels/Hue/Vietnam']
+                     'https://www.hostelworld.com/hostels/Hue/Vietnam']
 
 
 
@@ -38,9 +38,13 @@ class HostelworldSpider(Spider) :
 
 
     def parse_results_page(self, response) :
-        hostel_urls = response.xpath('//a[@class="hwta-property-link"]/@href').extract()
+        hostels = response.xpath('//a[@class="hwta-property-link"]/@href').extract()
         #hostel_urls = list(set(hostel_urls))[1:]
-        hostel_urls = hostel_urls[1::4]
+        hostel_urls = hostels[1::4]
+        if hostel_urls[0]=='':
+            hostel_urls = hostels[3::4]
+        ###### maybe add more ifs to make this comprehensive to even worse pages?
+
 
         print('='*55)
         print(len(hostel_urls))
