dee89675375a7c859fd36b75b6f4b60d346e7003
Zack Zbar <zackzbar@gmail.com>
2020-05-10 12:01:53 -0400

    Progress on Analysis. Moved out old data.

--- hostelworld/settings.py
+++ hostelworld/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'hostelworld.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36"
+#USER_AGENT = "INSERT UA HERE"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
